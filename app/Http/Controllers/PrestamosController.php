39182d46670db734164857b545d91211ac19ab23
Emmanuel Aguilar <emmanuelrar@gmail.com>
2017-10-05 22:10:59 -0400

    fix en sistema de simulacion. añadido backend de registro de prestamo.

--- app/Http/Controllers/PrestamosController.php
+++ app/Http/Controllers/PrestamosController.php
@@ -15,4 +15,19 @@ class PrestamosController extends Controller
 
         return view('credits.credits', compact('empleados'));
     }
+
+    public function insert(Request $request) {
+        $prestamos = new Prestamos();
+
+        $prestamos->cedula_empleado = $request->input('cedula');
+        $prestamos->monto = $request->input('monto');
+        $prestamos->tasa_interes = $request->input('tasa_interes');
+        $prestamos->monto_interes = $request->input('monto_interes');
+        $prestamos->cuotas = $request->input('cuotas');
+        $prestamos->cuotas_restantes = $request->input('cuotas_restantes');
+        $prestamos->fecha_final = Carbon::now();
+        $prestamos->save();
+
+        return response()->json(['message' => 'success']);
+    }
 }
